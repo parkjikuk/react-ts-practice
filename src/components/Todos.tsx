cd0ad1bcbe7ca32cade250489972c07adf35fba0
parkjikuk <paxkk@naver.com>
2023-01-11 02:04:45 +0900

    use context in Todos component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,18 @@
-import Todo from "../models/todo";
+import { TodosContext } from "../store/todos-context";
 import TodoItem from "./TodoItem";
+import classes from './Todos.module.css';
+import { useContext } from 'react';
 
-const Todos: React.FC<{ items: Todo[]}> = (props) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
-    <ul>
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoItem 
+        key={item.id} 
+        text={item.text} 
+        onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)} />
       ))}
     </ul>
   );
